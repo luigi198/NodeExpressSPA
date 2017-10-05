78b5992377b1ff00af7bf14e69102e87702dae67
Luigi <luis.cordoba198@gmail.com>
2017-10-05 09:29:36 -0600

    first commit

---
+++ config/routes.js
@@ -0,0 +1,19 @@
+// controllers
+// var main = require('../app/controllers/main')
+// var api = require('../app/controllers/api')
+
+// -----------------------
+// Expose
+// -----------------------
+
+module.exports = function(app) {
+  // SPA
+  app.get('*', function(req, res) {
+		res.sendFile(path.join(__dirname, 'public', 'layout.html'));
+	})
+
+  //
+  // API
+  //
+  // app.get('/api/helloworld', api.helloworld)
+}
